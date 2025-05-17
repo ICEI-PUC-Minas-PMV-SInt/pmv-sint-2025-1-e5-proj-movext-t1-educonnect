42798be973154e255a0f23cf16b7b4804a0408c4
T4RUM <wesley-murat@hotmail.com>
2025-05-16 21:05:40 -0300

    Desenvolvimento da tela de recuperar acesso

---
+++ src/app/(auth)/reset-password.tsx
@@ -0,0 +1,60 @@
+import CustomButton from "@/components/Button/Button";
+import { styles } from "@/lib/styles";
+import { useState, useCallback } from "react";
+import { Alert } from "react-native";
+import { Surface, Text, TextInput } from "react-native-paper";
+
+const Page = () => {
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const resetPassword = useCallback(async () => {
+    if (loading) return;
+
+    if (!email.includes("@")) {
+      Alert.alert("Email inválido", "Insira um email válido.");
+      return;
+    }
+
+   console.log("Reset de senha", email);
+ 
+  }, [email, loading]);
+
+  return (
+    <Surface style={{ ...styles.screen, alignItems: undefined }}>
+      <Text variant="headlineSmall" style={{ textAlign: "center" }}>
+        Recuperar Acesso
+      </Text>
+      <Text
+        style={{
+          fontSize: 16,
+          color: "#222823B3",
+          textAlign: "center",
+          marginBottom: 20,
+        }}
+      >
+        Para recuperar seu acesso, informe seu email e receba as instruções.
+      </Text>
+
+      <TextInput
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={{
+          marginVertical: 40,
+        }}
+      />
+
+      <CustomButton
+        text="Recuperar Senha"
+        onPress={resetPassword}
+        disabled={loading}
+      />
+    </Surface>
+  );
+};
+
+export default Page;
